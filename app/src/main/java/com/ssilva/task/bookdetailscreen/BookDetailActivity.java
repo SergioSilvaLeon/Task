35adae8f8f3b056de12fbe12f94c2f9f1252a97a
Sergio Silva <ssilva@nearsoft.com>
2018-04-20 16:39:26 -0700

    File Dir Restructure

---
+++ app/src/main/java/com/ssilva/task/bookdetailscreen/BookDetailActivity.java
@@ -0,0 +1,58 @@
+package com.ssilva.task.bookdetailscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.master.glideimageview.GlideImageView;
+import com.ssilva.task.booklist.MainActivity;
+import com.ssilva.task.R;
+import com.ssilva.task.model.Book;
+import com.ssilva.task.network.ApiClient;
+import com.ssilva.task.network.ApiInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BookDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_detail);
+
+        final GlideImageView bookCover = findViewById(R.id.imageViewBookCover);
+        final TextView tvPublishedDate = findViewById(R.id.tvPublishedDate);
+        final TextView tvAuthors = findViewById(R.id.tvAuthors);
+        final TextView tvDescription = findViewById(R.id.tvDescription);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra(MainActivity.EXTRA_BOOK_ID);
+
+        ApiInterface apiService =
+                ApiClient.retrofit().create(ApiInterface.class);
+
+        Call<Book> call = apiService.getBook(id);
+
+        call.enqueue(new Callback<Book>() {
+            @Override
+                public void onResponse(Call<Book> call, Response<Book> response) {
+                Book book = response.body();
+                bookCover.loadImageUrl(book.getVolumeInfo().getImageLinks().getLarge());
+                tvPublishedDate.setText(book.getVolumeInfo().getPublishedDate());
+                tvAuthors.setText(book.getVolumeInfo().getAuthors().toString());
+                tvDescription.setText(book.getVolumeInfo().getDescription());
+            }
+
+            @Override
+            public void onFailure(Call<Book> call, Throwable t) {
+                Toast.makeText(BookDetailActivity.this, "Error, on Connection!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/ssilva/task/booklist/BooksAdapter.java
@@ -0,0 +1,78 @@
+package com.ssilva.task.booklist;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.master.glideimageview.GlideImageView;
+import com.ssilva.task.R;
+import com.ssilva.task.bookdetailscreen.BookDetailActivity;
+import com.ssilva.task.model.Book;
+
+import java.util.List;
+
+
+public class BooksAdapter  extends RecyclerView.Adapter<BooksAdapter.BookViewAdapter>{
+
+    private List<Book> mBooks;
+    private Context mContext;
+
+    public BooksAdapter(List<Book> books, Context context){
+        mBooks = books;
+        mContext = context;
+    }
+
+    @Override
+    public BookViewAdapter onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout,
+                parent, false);
+
+        return new BookViewAdapter(view);
+    }
+
+    @Override
+    public void onBindViewHolder(BookViewAdapter holder, int position) {
+        Book book = mBooks.get(position);
+        holder.bindToView(book);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mBooks.size();
+    }
+
+    public class BookViewAdapter extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView bookTitle;
+        TextView bookProductionDate;
+        GlideImageView bookThumbnail;
+
+
+        public BookViewAdapter(View itemView) {
+            super(itemView);
+
+            bookTitle = itemView.findViewById(R.id.tvBookTitle);
+            bookProductionDate = itemView.findViewById(R.id.tvPublishedDate);
+            bookThumbnail = itemView.findViewById(R.id.imageViewThumbnail);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bindToView(Book book){
+            bookTitle.setText(book.getVolumeInfo().getTitle());
+            bookProductionDate.setText(book.getVolumeInfo().getPublishedDate());
+            bookThumbnail.loadImageUrl(book.getVolumeInfo().getImageLinks().getSmallThumbnail());
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent(mContext, BookDetailActivity.class);
+            intent.putExtra(MainActivity.EXTRA_BOOK_ID, mBooks.get(getAdapterPosition()).getId());
+            mContext.startActivity(intent);
+        }
+    }
+}
