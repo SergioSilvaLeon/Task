438215c4bdd294d26d4cb5bca7c07b2acb61a019
Sergio Silva <ssilva@nearsoft.com>
2018-05-11 17:30:56 -0700

    Autovalue gson dependency and BookList impl

--- app/src/main/java/com/ssilva/task/data/models/AutoValueGsonFactory.java
+++ app/src/main/java/com/ssilva/task/data/models/AutoValueGsonFactory.java
@@ -1,8 +1,15 @@
 package com.ssilva.task.data.models;
 
-/**
- * Created by sergiosilva on 5/11/18.
- */
+import com.google.gson.TypeAdapterFactory;
+import com.ryanharter.auto.value.gson.GsonTypeAdapterFactory;
 
-public class AutoValueGsonFactory {
+@GsonTypeAdapterFactory
+public abstract class AutoValueGsonFactory implements TypeAdapterFactory{
+
+    // Static factory method to access the package
+    // private generated implementation
+    public static TypeAdapterFactory create() {
+        return new AutoValueGson_AutoValueGsonFactory();
+    }
+
 }
--- app/src/main/java/com/ssilva/task/data/models/BookList.java
+++ app/src/main/java/com/ssilva/task/data/models/BookList.java
@@ -4,6 +4,8 @@ package com.ssilva.task.data.models;
 import java.util.List;
 
 import com.google.auto.value.AutoValue;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
 import com.google.gson.annotations.SerializedName;
 
 @AutoValue
@@ -11,5 +13,9 @@ public abstract class BookList {
 
     @SerializedName("items")
     public abstract List<Book> books();
+
+    public static TypeAdapter<BookList> typeAdapter(Gson gson) {
+        return new AutoValue_BookList.GsonTypeAdapter(gson);
+    }
 
 }
